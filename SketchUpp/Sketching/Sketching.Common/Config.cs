f69211a5bd679b8890a3827073c9d651bff8b38a
Xet <xet.erixon@gmail.com>
2016-11-23 10:03:19 +0100

    Some documentation and removed some unused stuff.

--- SketchUpp/Sketching/Sketching.Common/Config.cs
+++ SketchUpp/Sketching/Sketching.Common/Config.cs
@@ -3,8 +3,6 @@ namespace Sketching.Common
 {
 	public static class Config
 	{
-		public static double ScreenWidth { get; set; } = - 1.0;
-		public static double ScreeHeight { get; set; } = -1.0;
 		public static int GridSize { get; set; } = -1;
 	}
 }
