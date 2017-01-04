8512f05c5504c80cf261973399bfec6fe2042520
Torbjorn Eriksson <xet.erixon@gmail.com>
2017-01-04 09:23:01 +0100

    UWP

---
+++ SketchUpp/Sketching/Sketching.UWP/FixedRotationPageRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using Windows.Graphics.Display;
+using Windows.System.Profile;
+using Windows.UI.ViewManagement;
+using Sketching.Common.Pages;
+using Sketching.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+[assembly: ExportRenderer(typeof(FixedRotationPage), typeof(FixedRotationPageRenderer))]
+namespace Sketching.UWP
+{
+	class FixedRotationPageRenderer : PageRenderer
+	{
+		DisplayOrientations _prevOrientation = DisplayOrientations.None;
+		protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+		{
+			base.OnElementChanged(e);
+			if (e.NewElement != null)
+			{
+				if (!IsDesktop)
+				{
+					_prevOrientation = DisplayInformation.AutoRotationPreferences;
+					var page = (FixedRotationPage) Element;
+					var orientation = page?.Orientation;
+					if (orientation.HasValue && orientation.Value != PageOrientation.Default)
+					{
+						DisplayInformation.AutoRotationPreferences = orientation.Value == PageOrientation.Landscape
+							? DisplayOrientations.Landscape
+							: DisplayOrientations.Portrait;
+					}
+				}
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			DisplayInformation.AutoRotationPreferences = _prevOrientation;
+		}
+
+		protected override void OnDisconnectVisualChildren()
+		{
+			System.Diagnostics.Debug.WriteLine("Dis");
+			base.OnDisconnectVisualChildren();
+		}
+
+		private bool IsDesktop => UIViewSettings.GetForCurrentView().UserInteractionMode == UserInteractionMode.Mouse &&
+		                          AnalyticsInfo.VersionInfo.DeviceFamily == "Windows.Desktop";
+
+		protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+		}
+	}
+}
