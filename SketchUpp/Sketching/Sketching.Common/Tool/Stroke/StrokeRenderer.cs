2be0ac97e47f10481d0a94a5b458faac082af762
Daniel Cornelius <oneninty5@hotmail.se>
2017-01-19 11:51:00 +0100

    Highlight fix

--- SketchUpp/Sketching/Sketching.Common/Tool/Stroke/StrokeRenderer.cs
+++ SketchUpp/Sketching/Sketching.Common/Tool/Stroke/StrokeRenderer.cs
@@ -17,28 +17,30 @@ namespace Sketching.Tool.Stroke
 			if (stroke == null || !stroke.IsValid) return;
 			using (var paint = new SKPaint())
 			{
-				// Draw Stroke
 				paint.IsStroke = true;
 				paint.StrokeCap = SKStrokeCap.Round;
 				paint.StrokeWidth = (float)(stroke.Size * scale);
 				paint.IsAntialias = true;
 				paint.Color = stroke.Color.ToSkiaColor();
-				if (stroke.HighLight) paint.BlendMode = SKBlendMode.Darken; //TODO: Maybe use filters, paint.ColorFilter = SKColorFilter.CreateLighting()
+
 				var points = stroke.Points.Select(arg => Helper.Converter.ToSKPoint(arg, scale)).ToArray();
-				canvas.DrawPoints(SKPointMode.Polygon, points, paint);
+				// Draw HighLight or Stroke
+				if (stroke.HighLight)
+				{
+					paint.StrokeCap = SKStrokeCap.Butt;
+					paint.Color = stroke.Color.ToFillColor().ToSkiaColor();
+					canvas.DrawPointsInPath(paint, points);
+				}
+				else
+				{
+					canvas.DrawPoints(SKPointMode.Polygon, points, paint);
+				}
 				// Fill Stroke
 				if (!stroke.IsFilled) return;
 				if (!points.Any()) return;
 				paint.Color = stroke.Color.ToFillColor().ToSkiaColor();
 				paint.IsStroke = false;
-				var path = new SKPath();
-				foreach (var skPoint in points)
-				{
-					if (!path.Points.Any())
-						path.MoveTo(skPoint);
-					path.LineTo(skPoint);
-				}
-				canvas.DrawPath(path, paint);
+				canvas.DrawPointsInPath(paint, points);
 			}
 		}
 	}
